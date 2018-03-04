acda7849b0c71b3b00853892d276e21389cb2c90
Phillip <phillipables1@gmail.com>
2018-03-04 11:33:58 -0500

    main class extends recyclerView adapter type product view holder, product view holder class inside our class extends recyclerview viewholder, main class context and productlist set in product adapter constructor oncreate view holder for product view holder we have an instance of a layout inflater with our context and a view of that inflated layout with our resource list layout in it

--- app/src/main/java/com/example/techtron/myapplication/ProductAdapter.java
+++ app/src/main/java/com/example/techtron/myapplication/ProductAdapter.java
@@ -1,5 +1,43 @@
 package com.example.techtron.myapplication;
 
-public class ProductAdapter {
-    
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+//this class is our adapter
+public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductViewHolder>{
+
+    private Context mCtx;
+    private List<Product> productList;
+
+    public ProductAdapter(Context mCtx, List<Product> productList) {
+        this.mCtx = mCtx;
+        this.productList = productList;
+    }
+
+    @Override
+    public ProductViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(mCtx);
+        View view = inflater.inflate();
+    }
+
+    @Override
+    public void onBindViewHolder(ProductViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class ProductViewHolder extends RecyclerView.ViewHolder {
+        public ProductViewHolder(View itemView) {
+            super(itemView);
+        }  // this class is our viewholder
+    }
 }
