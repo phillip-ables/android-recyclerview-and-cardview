ef3a032353b0f6021ff09c889de9f9f1b47155c8
Phillip <phillipables1@gmail.com>
2018-03-04 12:37:29 -0500

    product list items added

--- app/src/main/java/com/example/techtron/myapplication/MainActivity.java
+++ app/src/main/java/com/example/techtron/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.techtron.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -23,5 +24,33 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
+
+        productList.add(
+                new Product(
+                        1,
+                        "Apple MacBook Air Core i5 5th Gen - (8 GB/128 GB SSD/Mac OS Sierra)",
+                        "13.3 inch, Silver, 1.35 kg",
+                        4.3,
+                        60000,
+                        R.drawable.mackbook));
+        productList.add(
+                new Product(
+                        2,
+                        "Dell Inspiron 7000 Core i5 7th Gen - (8GB/1 TB HDD/Windows 10 Home)",
+                        "14 inch, Gray, 1.659 kg",
+                        4.3,
+                        60000,
+                        R.drawable.dellinspiron));
+        productList.add(
+                new Product(
+                        3,
+                        "Microsoft Surface Pro 4 Core m3 6th Gen - (4 GB/128 GB SSD/Windows 10",
+                        "13.3 inch, Silver, 1.35kg",
+                        4.3,
+                        60000,
+                        R.drawable.surface));
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     }
 }
